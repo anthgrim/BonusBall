413c1b5ae82dd3626d3182e97f92e268bcc71cf7
anthgrim <anthgrim@icloud.com>
2022-02-05 00:40:46 -0500

    changes in JS

--- scripts.js
+++ scripts.js
@@ -1,14 +1,40 @@
+//Constants
 const ball = document.getElementById("ball");
+const button = document.getElementById("btn")
 let xIni = 0;
 let yIni = 0;
-const xMax = window.screen.width;
-const xMin = 0;
-const yMax = window.screen.height;
-const yMin = 0;
+let newBallY = 100;
+let xMax = window.screen.width;
+let xMin = 0;
+let yMax = window.screen.height;
+let yMin = 0;
 
+const newBall = document.createElement('div');
+
+
+//Generates random RGB Color
 const randomRGB = () => {
     const R = Math.floor(Math.random() * 255);
     const G = Math.floor(Math.random() * 255);
     const B = Math.floor(Math.random() * 255);
+    const RGB = `rgb(${R},${G},${B})`;
 
+    return RGB;
 }
+
+const changePosition = () =>{   
+    ball.style.backgroundColor = randomRGB();
+    ball.style.top = yMax + "px";
+    ball.style.left = xMax + "px";
+}
+
+setTimeout(changePosition,3000)
+
+//Interaction Interval
+// setInterval(()=>{
+//     
+// },1000)
+
+// button.addEventListener('click',()=>{
+    
+// })
