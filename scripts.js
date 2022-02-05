1c68c3407e74840616776cdd2d466bb2767b16ef
anthgrim <anthgrim@icloud.com>
2022-02-05 00:08:37 -0500

    Added Styles.css and added constants in JS

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const ball = document.getElementById("ball");
+let xIni = 0;
+let yIni = 0;
+const xMax = window.screen.width;
+const xMin = 0;
+const yMax = window.screen.height;
+const yMin = 0;
+
+const randomRGB = () => {
+    const R = Math.floor(Math.random() * 255);
+    const G = Math.floor(Math.random() * 255);
+    const B = Math.floor(Math.random() * 255);
+
+}
